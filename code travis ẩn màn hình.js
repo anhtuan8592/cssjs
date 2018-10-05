48f5f0fefb3e5fa496e3e1b0c6b94b65b022ff1a
ngô văn tuấn <ngovantuan66@gmail.com>
2018-10-05 15:26:02 +0700

    Update code travis ẩn màn hình.js

--- code travis ẩn màn hình.js
+++ code travis ẩn màn hình.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'https://datnenso.vn/dat-nen-long-an/';
+	var url_popup = 'http://datnenso.vn/dat-hoc-mon/';
 
       //<![CDATA[
-	  // var url_ppp = 'https://datnenso.vn/dat-nen-long-an/';
+	  // var url_ppp = 'http://datnenso.vn/dat-hoc-mon/';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
