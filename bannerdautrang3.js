c81895b789d6c130f3a181375ff88707dfe18eb7
ngô văn tuấn <ngovantuan66@gmail.com>
2018-10-06 08:45:59 +0700

    Update bannerdautrang3.js

--- bannerdautrang3.js
+++ bannerdautrang3.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a href='https://datnenso.vn/' target='_blank'><img src='https://datnenso.vn/wp-content/themes/datnenso/images/nha%20trang%20center.JPG' width='500px'/><br/>Nhận tư vấn miễn phí đón đầu các dự án</a>. đất nền số nơi trao gửi niềm tin <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a href='https://datnenso.vn/' target='_blank'><img src='https://4.bp.datnenso.vn/wp-content/themes/datnenso/images/nha%20trang%20center.JPG' width='500px'/><br/>Nhận tư vấn miễn phí đón đầu các dự án</a>. đất nền số nơi trao gửi niềm tin <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
